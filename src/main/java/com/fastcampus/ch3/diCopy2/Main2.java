1128a549582239d2f113a3295ed02884394c2007
miri yoon <meerie0419@gmail.com>
2023-10-30 17:28:41 +0900

    Spring DI Study

--- src/main/java/com/fastcampus/ch3/diCopy2/Main2.java
+++ src/main/java/com/fastcampus/ch3/diCopy2/Main2.java
@@ -1,6 +1,7 @@
 package com.fastcampus.ch3.diCopy2;
 
 import java.io.FileReader;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Properties;
@@ -14,9 +15,23 @@ class AppContext{
     Map map ; //　객체저장소
 
     AppContext() {
-        map = new HashMap();
-        map.put("car", new SportsCar());
-        map.put("Engine", new Engine());
+        try {
+            Properties p = new Properties();
+            p.load(new FileReader("config.txt"));
+
+            // Properties에 저장된 내용을 Map에 저장
+            map = new HashMap(p);
+            
+            // 반복문으로 클래스 이름을 얻어서 객체를 생성해서 다시 map에 저장
+            for(Object key : map.keySet()){
+                Class clazz = Class.forName((String)map.get(key));
+                map.put(key, clazz.newInstance());
+            }
+
+            Class clazz = Class.forName(p.getProperty("car"));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     Object getBean(String key) {
@@ -26,9 +41,11 @@ class AppContext{
 
 public class Main2 {
     public static void main(String[] args) throws Exception {
-        Car car = (Car)getObject("car");
-        Engine engine = (Engine) getObject("engine");
+        AppContext ac = new AppContext();
+        Car car = (Car)ac.getBean("car");
+        Engine engine = (Engine)ac.getBean("engine");
         System.out.println("car = " + car);
+        System.out.println("engine = " + engine);
     }
     static Object getObject(String key) throws Exception {
         Properties p = new Properties();

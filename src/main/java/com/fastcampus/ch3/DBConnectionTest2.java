aa218686e46fd554a18af9d8073c54004a237fa4
miri yoon <meerie0419@gmail.com>
2023-11-10 17:44:09 +0900

    Spring DI Study

--- src/main/java/com/fastcampus/ch3/DBConnectionTest2.java
+++ src/main/java/com/fastcampus/ch3/DBConnectionTest2.java
@@ -8,20 +8,6 @@ import java.sql.Connection;
 
 public class DBConnectionTest2 {
     public static void main(String[] args) throws Exception {
-        //        // 스키마의 이름(springbasic)이 다른 경우 알맞게 변경
-//        String DB_URL = "jdbc:mysql://localhost:3306/springbasic?useUnicode=true&characterEncoding=utf8";
-//
-//        // DB의 userid와 pwd를 알맞게 변경
-//        String DB_USER = "miri";
-//        String DB_PASSWORD = "realme3411^^";
-//        String DB_DRIVER = "com.mysql.jdbc.Driver";
-//
-//        DriverManagerDataSource ds = new DriverManagerDataSource();
-//        ds.setDriverClassName(DB_DRIVER);
-//        ds.setUrl(DB_URL);
-//        ds.setUsername(DB_USER);
-//        ds.setPassword(DB_PASSWORD);
-
         ApplicationContext ac = new GenericXmlApplicationContext("file:src/main/webapp/WEB-INF/spring/**/root-context.xml");
         DataSource ds = ac.getBean(DataSource.class);
 
